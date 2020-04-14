bb72b32d87dc90cf980e3006ff91145d06b3bc3f
Grigorii K. Shartsev <me@shgk.me>
2020-04-14 17:29:59 +0500

    feat: setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('Meetups API')
+    .addSecurity('cookie-session', {
+      type: 'apiKey',
+      in: 'cookie',
+      name: 'connected',
+    })
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
