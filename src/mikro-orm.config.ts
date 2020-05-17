56f08cd4f7cb7c72f4e8c67154fad82e923fc349
Grigorii K. Shartsev <me@shgk.me>
2020-05-18 03:38:28 +0500

    chore: disable mikro-orm cache

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -15,4 +15,5 @@ export default {
   autoFlush: false, // Read more here: https://mikro-orm.io/unit-of-work/
   baseDir: path.join(__dirname, '..'),
   logger: logger.log.bind(logger),
+  cache: { enabled: false },
 } as Options;
